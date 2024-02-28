35d529641b3790b888028892c50ab862739148d1
muvildan <danielvilches98@gmail.com>
2024-02-29 00:23:15 +0100

    Bonus: JS API call with chart ready 👨‍💻

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,47 @@
+// Function to fetch data from the API and display it on a chart
+export function displayCovidData(elementId) {
+    fetch('https://api.covidtracking.com/v1/us/daily.json')
+      .then(response => response.json())
+      .then(data => {
+        // Get data for the last 14 days
+        const lastTwoWeeksData = data.slice(0, 14);
 
+        // Split the data into last week and the week before
+        const lastWeekData = lastTwoWeeksData.slice(0, 7);
+        const weekBeforeLastData = lastTwoWeeksData.slice(7, 14);
+
+        // Extract positive cases for each week
+        const lastWeekCases = lastWeekData.map(day => day.positive);
+        const weekBeforeLastCases = weekBeforeLastData.map(day => day.positive);
+
+        // Reverse data to start from the oldest to the newest
+        lastWeekCases.reverse();
+        weekBeforeLastCases.reverse();
+        
+        // Display the Data on a Graph
+        const ctx = document.getElementById(elementId).getContext('2d');
+        new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: ['7 - 14', '6 - 13', '5 - 12', '4 - 11', '3 - 10', '2 - 9', '1 - 8'],
+            datasets: [
+              {
+                label: 'Cases Last Week',
+                data: lastWeekCases,
+                borderColor: 'rrgb(85, 77, 222)',
+                backgroundColor: 'rgb(85, 77, 222)',
+                borderWidth: 1
+              },
+              {
+                label: 'Cases Week Before Last',
+                data: weekBeforeLastCases,
+                borderColor: 'rgb(244, 78, 119)',
+                backgroundColor: 'rgb(244, 78, 119)',
+                borderWidth: 1
+              }
+            ]
+          },
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+}
